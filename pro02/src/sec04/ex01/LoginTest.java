98e5a00ef46ef2f439d5300d9a6fa331c40cd041
janghyeon <gold_kwak@naver.com>
2020-03-24 22:56:26 +0900

    0322

---
+++ pro02/src/sec04/ex01/LoginTest.java
@@ -0,0 +1,45 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/loginTest")
+public class LoginTest extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() call..");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		if((user_id == "" || user_id.length() == 0) || (user_pw == "" || user_pw.length() == 0)) {
+			out.print("<html>");
+			out.print("<body>");
+			out.print("ID 와 PW를 제대로 입력해주세요");
+			out.print("<a href='http://localhost:8080/pro02/test01/login.html'>로그인창으로 이동<a>");
+			out.print("</body>");
+			out.print("</html>");
+		} else {
+			out.print("아이디: " + user_id);
+			out.print("비밀번호: " + user_pw);
+		}		
+	}
+	
+	public void destroy() {
+		System.out.println("destroy() call..");
+	}
+}
