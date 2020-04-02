8928aa15a64d41dff5da3201299d3106d567cc03
janghyeon <gold_kwak@naver.com>
2020-04-02 23:42:42 +0900

    0402

--- pro04/src/sec04/ex03/MemberDAO.java
+++ pro04/src/sec04/ex03/MemberDAO.java
@@ -1,5 +1,18 @@
 package sec04.ex03;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.sql.DataSource;
+
 public class MemberDAO {
-	
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+		
+	public MemberDAO() {
+		try {
+			context ctx = new Inti
+		}
+	}
 }
--- pro04/src/sec04/ex03/MemberServlet.java
+++ pro04/src/sec04/ex03/MemberServlet.java
@@ -2,15 +2,17 @@ package sec04.ex03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("member")
+@WebServlet("/member")
 public class MemberServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -27,7 +29,11 @@ public class MemberServlet extends HttpServlet{
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		MemberDAO dao = new MemberDAO();
-		List<E> memberList = 
+		List membersList = dao.listMembers();
+		request.setAttribute("membersList", membersList);
+		RequestDispatcher dispatch = request.getRequestDispatcher("viewMembers");
+		dispatch.forward(request, response);
+		
 	}
 	
 }
